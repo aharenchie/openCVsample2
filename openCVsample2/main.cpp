340e974bb25e377599b99554fb86410621c73e36
aharenchie <e125731@ie.u-ryukyu.ac.jp>
2015-02-01 03:27:11 +0900

    完成

--- openCVsample2/main.cpp
+++ openCVsample2/main.cpp
@@ -11,67 +11,69 @@
 
 using namespace cv;
 
-int
-main (int argc, char **argv)
-{
-    // (1)load a specified file and convert it into grayscale image
-    IplImage* src_img = cvLoadImage("/Users/e125731/Desktop/openCVsample2/pic/写真.JPG");
-    IplImage *gray_img =cvCreateImage( cvGetSize(src_img),IPL_DEPTH_8U,1);
+int main (int argc, char **argv){
     
-    cvCvtColor(src_img, gray_img, CV_BGR2GRAY);
-    cvSmooth(gray_img, gray_img, CV_GAUSSIAN, 5, 5, 0, 0);//平滑化
+    int vertices = 4;
+    int object_num = 0;
+    float x = 0,y = 0;
+    CvMemStorage *storage = cvCreateMemStorage (0);
+    CvSeq* contours = 0;
+    CvSeq* result;
+    CvPoint* pt[vertices];
 
-    // (2)apply a fixed-level threshold to each pixel
- 
+    
+    //画像の読み込み
+    IplImage* src_img = cvLoadImage("/Users/e125731/Desktop/openCVsample2/pic/写真.JPG");
+    
+    IplImage *gray_img =cvCreateImage(cvGetSize(src_img),IPL_DEPTH_8U,1);
+    
+    // 平滑化
+    cvCvtColor(src_img, gray_img, CV_BGR2GRAY);//グレー化
+    cvSmooth(gray_img, gray_img, CV_GAUSSIAN, 5, 5, 0, 0);//平滑化(ガウシアン処理)
+
+    // 二値化
     cvThreshold(gray_img, gray_img, 0, 255, THRESH_TRUNC|THRESH_OTSU);
     cvThreshold(gray_img, gray_img, 0, 255, THRESH_BINARY|THRESH_OTSU);
 
-    
-    CvMemStorage *storage = cvCreateMemStorage (0);
-    CvSeq *contours = 0;
-
-
     //輪郭検出
-     cvFindContours(gray_img, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
+    cvFindContours(gray_img, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
     
-    CvSeq* result;
-    float x = 0,y = 0;
-    int vertices = 4;
-
+    //検出した輪郭から折れ線を検出する
     while(contours){
+        
+        //折れ線のもとになる線を検出する(Douglas-Peuckerを利用)
         result = cvApproxPoly(contours, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(contours)*0.05, 0);
         
-        
-        //if there are 4 vertices in the contour(It should be a quadrilateral)
+        //折れ線のもとになる線の数が4つのとき
        if(result->total==vertices  && abs(cvContourArea(result, CV_WHOLE_SEQ))>100)
         {
-            //iterating through each point
-            CvPoint *pt[vertices];
-            for(int i=0;i<vertices;i++){
+         
+            for(int i=0;i<vertices;i++)
                 pt[i] = (CvPoint*)cvGetSeqElem(result, i);
-            }
-    
+            
             if(abs(pt[1]->y - pt[0]->y) < 500){
                 
-                x = (pt[0]->x + pt[1]->x + pt[2]->x + pt[3]->x )/4;
-                y = (pt[0]->y + pt[1]->y + pt[2]->y + pt[3]->y )/4;
-                cvCircle(src_img,cvPoint(x,y), 5, CV_RGB(255, 0,0), -1);
+                x = (pt[0]->x + pt[1]->x + pt[2]->x + pt[3]->x )/4; //四角の中心座標xを求める
+                y = (pt[0]->y + pt[1]->y + pt[2]->y + pt[3]->y )/4; //四角の中心座標yを求める
                 
+                cvCircle(src_img,cvPoint(x,y), 5, CV_RGB(255,0,0), -1);
+                
+                object_num++;
+                printf("---%d個目---\n",object_num);
+                printf("(%d ,%d)\n",(int)x,(int)y);
             }
         }
        
-        contours = contours->h_next;
+        contours = contours->h_next;//次の輪郭を調べる
 
-        
     }
-      // (4)画像の表示
-    cvNamedWindow ("Level:1", CV_WINDOW_AUTOSIZE);
-    cvShowImage ("Level:1",src_img);
-    cvWaitKey (0);
     
+    // (4)画像の表示
+    cvNamedWindow ("result", CV_WINDOW_AUTOSIZE);
+    cvShowImage ("result",src_img);
+    cvWaitKey (0);
 
-    cvDestroyWindow ("Level:1");
- 
+    cvDestroyWindow ("result");
     
     return 0;
 }
